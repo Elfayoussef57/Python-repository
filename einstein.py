878919ca8e78fe03861562e51c4ea42752f3347c
Elfayoussef57 <pr.elfajriyoussef.57@gmail.com>
2024-12-02 21:00:34 +0100

    Delete einstein.py

--- einstein.py
+++
@@ -1,7 +0,0 @@
-def formula(masse):
-    energie = int(masse)*300000000*300000000
-    print(f"{energie}")
-def main():
-    m = int(input("enter the masse of your substance m: "))
-    formula(m)
-main()
