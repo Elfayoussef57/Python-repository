d8bb446b857a69945d5a31e1edceaf345e1be94d
Elfayoussef57 <pr.elfajriyoussef.57@gmail.com>
2024-12-02 21:00:46 +0100

    Delete faces.py

--- faces.py
+++
@@ -1,7 +0,0 @@
-def convert(enter):
-    enter = enter.replace(":)","🙂").replace(":(","☹️")
-    print(enter)
-def main():
-    name = input("enter a sentance that finish with ':)' or ':(' : ")
-    convert(name)
-main()
