289c82a1f031774440999c1b81b1a7a59cc2717e
Elfayoussef57 <pr.elfajriyoussef.57@gmail.com>
2025-01-02 00:48:59 +0100

    Update wordle.py

--- wordle_lab-main/wordle_package/wordle.py
+++ wordle_lab-main/wordle_package/wordle.py
@@ -7,10 +7,10 @@ Module for the Wordle game.
     - feed_back_word: Provides feedback on a guess compared to the word.
 
     Constants:
-    - MAX_ATTEMPTS: The maximum number of attempts allowed in the game.
+    - MAX_ATTEMPTS: The maximum number of attempts allowed in the game
 """
 from typing import List
-from colorama import Fore
+#we can import the colorama library if we dont know the color code
 
 # Max attempts of the game
 
@@ -25,37 +25,33 @@ def check_guess_correct(word: str, guess: str) -> bool:
     if guess == word  and MAX_ATTEMPTS >= 0:
         return True
     return False
-
-def contain(character, word):
-    test = 0
-    for i in range(len(word)):
-        if character == word:
-            test += 1
-    return test
     
 def feed_back_word(word: str, guess: str) -> List[str]:
     """Check the guess against the word and return the feedback
     Return: a list with the feedback for each letter in the guess
     """
-    count = 0
-    feed_back = []
-    # Initialize the feedback list
-    # It will contain the colors for each letter in the guess
-    # The default color is RED for all the letters
-    # GREEN for the correct letter in the correct position
-    # YELLOW for the correct letter in the wrong position
-    for c in word:
-        if c == guess[count]:
-            feed_back += Fore.GREEN + c + Fore.RESET
-            count += 1
-        elif c in guess and c != guess[count] and ((c not in feed_back) or contain(c, word) == 2):
-                feed_back += Fore.YELLOW + c + Fore.RESET
-                count += 1
+    feedback = []
+    word_list = list(word)
+    
+    for i in range(len(guess)):
+        if guess[i] == word[i]:
+            #green
+            feedback.append('\033[92m' + guess[i] + '\033[0m')
+            word_list[i] = None  # Mark as used
         else:
-            feed_back += Fore.RED + c + Fore.RESET
-            count += 1
-                
-    return feed_back
+            feedback.append(None)
+
+    for i in range(len(guess)):
+        if feedback[i] is None:
+            if guess[i] in word_list:
+                #yellow
+                feedback[i] = '\033[93m' + guess[i] + '\033[0m' 
+                word_list[word_list.index(guess[i])] = None
+            else:
+                #red
+                feedback[i] = '\033[91m' + guess[i] + '\033[0m'
+
+    return feedback
 
 def main():
     if check_guess_correct("admin", "abled"):
@@ -69,4 +65,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
